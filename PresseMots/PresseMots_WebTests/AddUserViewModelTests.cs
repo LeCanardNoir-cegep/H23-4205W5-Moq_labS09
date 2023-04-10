cc8e75c338853188cac5a32f7655a07df2ed713a
Bruno Pouliot <2168516@cegepmontpetit.ca>
2023-04-10 12:52:40 -0400

    FCT: Ajout du test de validation de AddUserViewModel

--- PresseMots/PresseMots_WebTests/AddUserViewModelTests.cs
+++ PresseMots/PresseMots_WebTests/AddUserViewModelTests.cs
@@ -22,19 +22,19 @@ namespace PresseMots_WebTests
         {
             /** ARRANGE */
             // Mock le provider de traduction du ViewModel AddUserViewModel.
-            var _localsMobk = new Mock<IStringLocalizer<AddUserViewModel>>();
-            var errorMessage = "Passwords are not string enough.";
-            _localsMobk.Setup( _ => _[errorMessage]).Returns( new LocalizedString(errorMessage, errorMessage) );
+            var mockLocals = new Mock<IStringLocalizer<AddUserViewModel>>();
+            var errorMessage = "Passwords are not strong enough.";
+            mockLocals.Setup( _ => _[errorMessage]).Returns( new LocalizedString(errorMessage, errorMessage) );
 
             // Mock le provider service
             // Injection de dépendance de la traduction
-            var services = new Mock<IServiceProvider>();
-            services.Setup(s => s.GetService(typeof(IStringLocalizer<AddUserViewModel>))).Returns(_localsMobk.Object);
+            var mockServices = new Mock<IServiceProvider>();
+            mockServices.Setup(s => s.GetService(typeof(IStringLocalizer<AddUserViewModel>))).Returns(mockLocals.Object);
 
             // Création du context de validation
-            // On a besoin de l'entité à valider et des services requis.
+            // On a besoin de l'entité à valider et des mockServices requis.
             // Ici nous avons besoin du service de traduction inclu dans les ServicesProvider
-            var validationContext = new ValidationContext(entity, services.Object, null);
+            var validationContext = new ValidationContext(entity, mockServices.Object, null);
             var results = new List<ValidationResult>();
 
             /** ACT */
@@ -52,23 +52,27 @@ namespace PresseMots_WebTests
         public void UsernameTooLong()
         {
             /** ARRANGE */
-            var entity = new AddUserViewModel
-            {
-                Username = new string('x', 200)
-            };
-
-            //var expectedMessage = string.Format(DefaultValidationMessages);
+            var entity = new AddUserViewModel(){ Username = new string('x', 200) };
+            var expectedMessage = string.Format(DefaultValidationMessages.StringLengthAttribute_ValidationError, "Username", 100);
 
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.Username));
+
             /** ASSERT */
+            Assert.Collection(error, x => Assert.Equal(expectedMessage, x.ErrorMessage));
         }
 
         [Fact]
         public void UsernameValid()
         {
             /** ARRANGE */
+            var entity = new AddUserViewModel(){ Username = new string('x', 99) };
+
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.Username));
+
             /** ASSERT */
+            Assert.Empty(error);
         }
 
         [Theory]
@@ -80,8 +84,14 @@ namespace PresseMots_WebTests
         public void EmailMalformed(string email)
         {
             /** ARRANGE */
+            var entity = new AddUserViewModel(){ Email = email };
+            var expectedMessage = string.Format(DefaultValidationMessages.EmailAddressAttribute_Invalid, "Email");
+
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.Email));
+
             /** ASSERT */
+            Assert.Collection(error, x => Assert.Equal(expectedMessage, x.ErrorMessage));
         }
 
         [Theory]
@@ -91,32 +101,54 @@ namespace PresseMots_WebTests
         public void EmailValid(string email)
         {
             /** ARRANGE */
+            var entity = new AddUserViewModel(){ Email = email };
+
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.Email));
+
             /** ASSERT */
+            Assert.Empty(error);
         }
 
         [Fact]
         public void PasswordsDontMatch()
         {
             /** ARRANGE */
+            var entity = new AddUserViewModel() { Password = "1234", ConfirmPassword = "4321" };
+            var expectedMessage = "Passwords don't match";
+
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.ConfirmPassword));
+
             /** ASSERT */
+            Assert.Collection(error, x => Assert.Equal(expectedMessage, x.ErrorMessage));
         }
 
         [Fact]
         public void PasswordsTooWeak()
         {
             /** ARRANGE */
+            var entity = new AddUserViewModel() { Password = "1234" };
+            var expectedMessage = "Passwords are not strong enough.";
+
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.Password));
+
             /** ASSERT */
+            Assert.Collection(error, x => Assert.Equal(expectedMessage, x.ErrorMessage));
         }
 
         [Fact]
         public void PasswordsValid()
         {
             /** ARRANGE */
+            var entity = new AddUserViewModel() { Password = "asdf211wERRE&?" };
+
             /** ACT */
+            var error = ValidateEntity(entity, nameof(entity.Password));
+
             /** ASSERT */
+            Assert.Empty(error);
         }
 
     }
